6cfe99ddb49adf2ddf98db0e3ec2b66640a38b53
Taw-ssif <mdabutawsif2k26@gmail.com>
2025-12-25 11:51:46 +0600

    Create Bondchecker.py

---
+++ Bondchecker.py
@@ -0,0 +1,36 @@
+def check_bond():
+    while True:
+        a = input("Enter bond number to check: ")
+        if a == '0':
+            break
+        elif len(a) != 7:
+            print("Bond number must be 7 digits")
+            continue
+        else:
+            with open("bonds.txt", "r") as f:
+                bonds = f.read().splitlines()
+                if a in bonds:
+                    print("You won this bond!: ", a)
+
+def addBond():
+    bond = []
+    while True:
+        a = input("Bond number(0 to end): ")
+        if a == '0':
+            break
+        elif len(a) != 7:
+            print("Bond number must be 7 digits")
+            continue
+        else:
+            bond.append(a)
+    with open("bonds.txt", "a") as f:
+        for b in bond:
+            f.write(b + "\n")
+    return bond
+
+print("Welcome to the Bond Checker")
+checker = input("Do you want to (1) Check a bond or (2) Add a bond?: ")
+if checker == '1':
+    check_bond()
+elif checker == '2':
+    addBond()
